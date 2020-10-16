70327d3d20eb51134ba3ad0d325cf85a7fd19683
rykz-s <ryukazuas@gmail.com>
2020-10-16 22:55:39 +0700

    commit

---
+++ quiz1/mainfood.js
@@ -0,0 +1,30 @@
+const items = [
+    'We',
+    'Have',
+    'Delicious',
+    'Food',
+    'Here'
+];
+
+const app = document.getElementById('app');
+let count = 0;
+let index = 0;
+let typingEffect = () => {
+    let text = items[index];
+    if (count < text.length) {
+        setTimeout(() => {
+            app.innerHTML += text[count];
+            count++;
+            typingEffect();
+        }, Math.floor(Math.random(10) * 100));
+    } else {
+        count = 0;
+        index = index + 1 < items.length ? index + 1 : 0;
+        setTimeout(() => {
+            app.innerHTML = '';
+            typingEffect();
+        }, 1500);
+    }
+};
+
+typingEffect();
